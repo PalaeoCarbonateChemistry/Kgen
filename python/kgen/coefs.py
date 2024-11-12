ac89ab62cadfdee345958ee7c1665e63fed989e8
Oscar Branson <ob266@cam.ac.uk>
2024-11-12 17:01:50 +0000

    correct use of importlib

--- python/kgen/coefs.py
+++ python/kgen/coefs.py
@@ -2,12 +2,10 @@ import json
 import os
 import importlib.resources as importlib_resources
 
-# import pkg_resources as pkgrs
+coef_path = importlib_resources.files('kgen') / 'coefficients'
 
-with importlib_resources.path('kgen', 'coefficients') as coef_path:
+with open(os.path.join(coef_path, 'K_calculation.json'), 'r') as f:
+    K_coefs = json.load(f)['coefficients']
 
-    with open(os.path.join(coef_path, 'K_calculation.json'), 'r') as f:
-        K_coefs = json.load(f)['coefficients']
-
-    with open(os.path.join(coef_path, 'K_pressure_correction.json'), 'r') as f:
-        K_presscorr_coefs = json.load(f)['coefficients']
+with open(os.path.join(coef_path, 'K_pressure_correction.json'), 'r') as f:
+    K_presscorr_coefs = json.load(f)['coefficients']
