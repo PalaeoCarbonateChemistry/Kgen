80917131a7953306141523dace179ed28585b6c1
Oscar Branson <ob266@cam.ac.uk>
2025-07-11 11:37:13 +0100

    move coefficients to zzz

--- r/R/zzz.R
+++ r/R/zzz.R
@@ -1,7 +1,20 @@
 # Select pyMyAMI version
 pymyami_version <- "2.1.1"
 
+.kgen_env <- new.env(parent = emptyenv())
+
 .onLoad <- function(...) {
+  # Load K coefficients
+  k_data <- rjson::fromJSON(file = system.file("coefficients/K_calculation.json", package = "kgen"))
+  .kgen_env$K_coefs <- k_data$coefficients
+  
+  # Load pressure correction coefficients
+  pc_data <- rjson::fromJSON(file = system.file("coefficients/K_pressure_correction.json", package = "kgen"))
+  .kgen_env$K_presscorr_coefs <- pc_data$coefficients
+  
+  # Load pymyami polynomial coefficients
+  .kgen_env$poly_coefs <- rjson::fromJSON(file = system.file("coefficients/polynomial_coefficients.json", package = "kgen"))
+
   reticulate::py_require(paste0("pymyami==", pymyami_version))
   pymyami <<- reticulate::import("pymyami", delay_load = TRUE)
 }
