e89e91fe5526d6730e397814bbc57151e7be1925
Oscar Branson <ob266@cam.ac.uk>
2025-07-11 09:41:50 +0100

    pkgname

--- r/R/coefficients.R
+++ r/R/coefficients.R
@@ -4,15 +4,15 @@ poly_coefs <- NULL
 
 .onLoad <- function(libname, pkgname) {
   # Load K coefficients
-  K_coefs <<- rjson::fromJSON(file = system.file("coefficients/K_calculation.json", package = "kgen"))
+  K_coefs <<- rjson::fromJSON(file = system.file("coefficients/K_calculation.json", package = pkgname))
   K_coefs <<- K_coefs$coefficients
   
   # Load pressure correction coefficients
-  K_presscorr_coefs <<- rjson::fromJSON(file = system.file("coefficients/K_pressure_correction.json", package = "kgen"))
+  K_presscorr_coefs <<- rjson::fromJSON(file = system.file("coefficients/K_pressure_correction.json", package = pkgname))
   K_presscorr_coefs <<- K_presscorr_coefs$coefficients
   
   # Load pymyami polynomial coefficients
-  poly_coefs <- rjson::fromJSON(file = system.file("coefficients/polynomial_coefficients.json", package = "kgen"))
+  poly_coefs <<- rjson::fromJSON(file = system.file("coefficients/polynomial_coefficients.json", package = pkgname))
 
   invisible()
 }
